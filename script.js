8ce91f0c4cec451e3dfd6e4182e126f4f073d587
Saksham <atomixdes@icloud.com>
2024-05-25 20:59:39 -0400

    Add eventListener for button to change canvas size.

--- script.js
+++ script.js
@@ -9,15 +9,38 @@ function createGrid() {
         div.style.height = numGrids + "px";
         div.style.width = numGrids + "px";
         container.appendChild(div);
-        console.log("Created Square #" + (i + 1));
     }
 }
 
-createGrid();
-const box = document.querySelectorAll(".square");
-
-box.forEach(square => {
-    square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = "black";
+function listener() {
+    const box = document.querySelectorAll(".square");
+    box.forEach(square => {
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "black";
+        })
     })
+}
+
+createGrid();
+listener();
+
+
+const but = document.querySelector(".size-button");
+
+but.addEventListener("click", () => {
+    while (1) {
+        let tempGridSize = Number(prompt("How many squares per side would you like? (Max 100): "));
+
+        if (tempGridSize == null || tempGridSize === NaN || tempGridSize > 100) {
+            alert("Please enter a valid number (Max 100)");
+            continue;
+        }
+
+        gridSize = tempGridSize;
+        numGrids = 960 / gridSize;
+        container.replaceChildren();
+        createGrid();
+        listener();
+        break;
+    }
 })
